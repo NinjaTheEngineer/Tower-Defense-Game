cbcda0d72edc05452228d1684818214c36b4586b
Alexandre Rocha Coutinho <alex.rochacoutinho@gmail.com>
2021-03-24 02:02:52 +0100

    Building Placements and HQ

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -10,13 +10,20 @@ public class BuildingManager : MonoBehaviour
     public static BuildingManager Instance { get; private set; }
 
     public event EventHandler<OnActiveBuildingTypeChangedEventArgs> OnActiveBuildingTypeChanged;
+    public event EventHandler<OnBuildingPlacementTrialEventArgs> OnBuildingPlacementTrial;
 
     public class OnActiveBuildingTypeChangedEventArgs : EventArgs
     {
         public BuildingTypeSO activeBuildingType;
     }
+    public class OnBuildingPlacementTrialEventArgs : EventArgs
+    {
+        public bool canBePlaced;
+    }
 
     private Camera mainCamera;
+
+    private bool canBuild;
 
     private BuildingTypeListSO buildingTypeList;
     private BuildingTypeSO activeBuildingType;
@@ -35,10 +42,10 @@ public class BuildingManager : MonoBehaviour
     private void Update()
     {
         //mouseVisualTransform.position = GetMouseWorldPosition();
-
+        canBuild = CanSpawnBuilding(activeBuildingType, UtilsClass.GetMouseWorldPosition());
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            if (activeBuildingType != null)
+            if (activeBuildingType != null && canBuild)
             {
                 Instantiate(activeBuildingType.prefab, UtilsClass.GetMouseWorldPosition(), Quaternion.identity);
             }
@@ -57,5 +64,63 @@ public class BuildingManager : MonoBehaviour
     public BuildingTypeSO GetActiveBuildingType()
     {
         return activeBuildingType;
+    }
+
+    private bool CanSpawnBuilding(BuildingTypeSO buildingType, Vector3 position)
+    {
+        if(buildingType == null)
+        {
+            return false;
+        }
+        BoxCollider2D boxCollider2D = buildingType.prefab.GetComponent<BoxCollider2D>();
+
+        Collider2D[] collider2DArray = Physics2D.OverlapBoxAll(position + (Vector3)boxCollider2D.offset, boxCollider2D.size, 0);
+
+        bool isAreaClear = collider2DArray.Length == 0;
+        if (!isAreaClear)
+        {
+            OnBuildingPlacementTrial?.Invoke(this,
+                new OnBuildingPlacementTrialEventArgs { canBePlaced = false });
+            return false;
+        }
+
+
+        collider2DArray = Physics2D.OverlapCircleAll(position, buildingType.minConstructionRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            //Colliders inside the construction raidus;
+            BuildingTypeHolder buildingTypeHolder = collider2D.GetComponent<BuildingTypeHolder>();
+
+            if (buildingTypeHolder != null)
+            {
+                if (buildingTypeHolder.buildingType == buildingType)
+                {
+                    OnBuildingPlacementTrial?.Invoke(this,
+                        new OnBuildingPlacementTrialEventArgs { canBePlaced = false });
+                    return false;
+                }
+            }
+        }
+
+        float maxConstructionRadius = 25f;
+        collider2DArray = Physics2D.OverlapCircleAll(position, maxConstructionRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            //Colliders inside the construction raidus;
+            BuildingTypeHolder buildingTypeHolder = collider2D.GetComponent<BuildingTypeHolder>();
+
+            if (buildingTypeHolder != null)
+            {
+                OnBuildingPlacementTrial?.Invoke(this,
+                    new OnBuildingPlacementTrialEventArgs { canBePlaced = true });
+                return true;
+            }
+        }
+
+        OnBuildingPlacementTrial?.Invoke(this,
+            new OnBuildingPlacementTrialEventArgs { canBePlaced = false });
+        return false;
     }
 }
